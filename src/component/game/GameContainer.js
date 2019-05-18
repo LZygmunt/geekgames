58e2d1108a841afca2a874dda59f3f67c278aa7e
RowickaM <rowickam.23@gmail.com>
2019-05-18 22:56:44 +0200

    lista gier

--- src/component/game/GameContainer.js
+++ src/component/game/GameContainer.js
@@ -5,21 +5,54 @@ import { compose } from "redux";
 import { firestoreConnect } from "react-redux-firebase";
 
 import "./game.css"
+import "./game-list.css"
 
 const GameContainer = ({ games, auth, followers }) => {
   console.log(games);
+    window.onscroll = function(){
+        var header = document.getElementById("game-header-table");
+        if(header!=null) {
+            var sticky = header.offsetTop;
+
+            if (window.pageYOffset > sticky) {
+                header.className = "sticky";
+            } else {
+                header.className = "";
+            }
+        }
+    };
   return (auth.uid) ? (
-    <GameList games={games} followers={followers}/>
+    <div className="game-list">
+        <div id="game-header-table">
+        <div className="game-search">
+            <input type="text" placeholder="Zacznij pisać, aby wyszukać grę" />
+        </div>
+
+        <div className="game-item">
+            <div className="title">Nazwa gry</div>
+            <div className="create-date">Data stworzenia</div>
+            <div className="author">Nick autora</div>
+            <div className="num-of-user">
+                Obserwujący
+            </div>
+            <div className="num-of-post">Posty</div>
+            <div className="num-of-event">Wydarzenia</div>
+        </div>
+        </div>
+      <GameList games={games} followers={followers}/>
+    </div>
   ): (<div/>);
 };
 
 const mapStoreToProps = (state) => {
-  console.log("state ordered -> ", state.firestore)
+  console.log("state ordered -> ", state.firestore);
   return {
     auth: state.firebase.auth,
     games: state.firestore.ordered.games
   }
 };
+
+
 
 export default compose(
   connect(mapStoreToProps),
--- src/component/game/GameList.js
+++ src/component/game/GameList.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const GameList = ({ games }) => {
-  return (
-    <div className="game-list">
-      {games && games.map(game => {
-        return (<Link to={"/game/" + game.id} key={game.id}>
-          <div>
-            {game.title}
-          </div>
-        </Link>)
-      })}
-    </div>
-  );
+const GameList = ({games}) => {
+    return (
+        <div className="game-element">
+            {games && games.map(game => {
+                return (
+                    <Link to={"/game/" + game.id} key={game.id}>
+                        <div className="game-item">
+                            <div className="title"> {game.title} </div>
+                            <div className="create-date"> {game.created.toDate().toLocaleDateString()}</div>
+                            <div className="author"> {game.authorNick}</div>
+                            <div className="num-of-user">
+                                <i className="far fa-user-circle"/>
+                                <i className="far fa-user-circle"/>
+                                <i className="far fa-user-circle"/>
+                                <p className="num-of-user-more"> +5</p>
+                            </div>
+                            <div className="num-of-post"> 16</div>
+                            <div className="num-of-event"> 4</div>
+                        </div>
+                    </Link>)
+            })}
+        </div>
+    );
 };
 
 export default GameList;
