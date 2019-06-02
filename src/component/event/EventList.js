11e5c94fb3418d282b3cc5939b27b75155f6b20b
RowickaM <rowickam.23@gmail.com>
2019-06-02 23:36:08 +0200

    zmiana stylu wyświetlania gry i wydarzeń w liście

--- src/component/event/EventList.js
+++ src/component/event/EventList.js
@@ -3,40 +3,37 @@ import { Link } from "react-router-dom";
 
 const EventList = ({ events }) => {
   //TODO ilość obserwujących
-  return (<div className="game-list">
-    <div id="game-header-table" style={{width:"100%"}}>
-      <div className="game-search">
+  return (<table className="game-list" cellSpacing="0">
+    <thead id="game-header-table" style={{width:"100%"}}>
+      <tr><td className="game-search" colspan="5" >
         <input type="text" placeholder="Zacznij pisać, aby wyszukać grę" />
-      </div>
-      <div className="game-item">
-        <div className="title">Nazwa wydarzenia</div>
-        <div className="place">Miejsce</div>
-        <div className="game-title">Nazwa gry</div>
-        <div className="start-date">Data rozpoczęcia</div>
-        <div className="author">Nick autora</div>
-        <div className="num-of-user">Obserwujący</div>
-      </div>
-    </div>
+      </td>
+      </tr>
+      <tr className="game-item">
+        <td className="title">Nazwa wydarzenia</td>
+        <td className="place">Miejsce</td>
+        <td className="game-title">Nazwa gry</td>
+        <td className="start-date">Data rozpoczęcia</td>
+        <td className="author">Nick autora</td>
+      </tr>
+    </thead><tbody>
     { events && events.map(event => {
-      return (<div className="game-element" key={ event.id }>
-        <Link to={ "/event/" + event.id }>
-          <div className="game-item">
-            <div className="title"> { event.title } </div>
-            <div className="place"> {event.place} </div>
-            <div className="game-title">{ event.gameTitle }</div>
-            <div className="start-date"> {event.startDate}</div>
-            <div className="author"> { event.authorNick }</div>
-            <div className="num-of-user">
-              <i className="far fa-user-circle"/>
-              <i className="far fa-user-circle"/>
-              <i className="far fa-user-circle"/>
-              <p className="num-of-user-more"> +5</p>
-            </div>
-          </div>
-        </Link>
-      </div>)
+      return (<tr className="game-element" key={ event.id }>
+
+            <td className="title"><Link to={ "/event/" + event.id }> { event.title }</Link> </td>
+            <td className="place"> {event.place} </td>
+            <td className="game-title">{ event.gameTitle }</td>
+            <td className="start-date"> {event.startDate}</td>
+            <td className="author"> { event.authorNick }</td>
+            {/*<td className="num-of-user">*/}
+            {/*  <i className="far fa-user-circle"/>*/}
+            {/*  <i className="far fa-user-circle"/>*/}
+            {/*  <i className="far fa-user-circle"/>*/}
+            {/*  <p className="num-of-user-more"> +5</p>*/}
+            {/*</td>*/}
+      </tr>)
   })}
-  </div>);
+  </tbody></table>);
 };
 
 export default EventList;
--- src/component/game/GameList.js
+++ src/component/game/GameList.js
@@ -3,42 +3,31 @@ import { Link } from "react-router-dom";
 
 const GameList = ({ games }) => {
   //TODO wprowadzić ilość obserwujących, postów oraz wydarzeń
-  return (<div className="game-list">
-    <div id="game-header-table" style={{width:"100%"}}>
-      <div className="game-search">
+  return (<table className="game-list" cellSpacing="0">
+    <thead id="game-header-table" style={{width:"100%"}}>
+    <tr>
+      <td className="game-search" colspan="3" >
         <input type="text" placeholder="Zacznij pisać, aby wyszukać grę" />
-      </div>
-      <div className="game-item">
-        <div className="title">Nazwa gry</div>
-        <div className="create-date">Data stworzenia</div>
-        <div className="author">Nick autora</div>
-        <div className="num-of-user">
-          Obserwujący
-        </div>
-        <div className="num-of-post">Posty</div>
-        <div className="num-of-event">Wydarzenia</div>
-      </div>
-    </div>
+      </td>
+    </tr>
+    <tr className="game-item">
+        <td className="title">Nazwa gry</td>
+        <td className="create-date">Data stworzenia</td>
+        <td className="author">Nick autora</td>
+      </tr>
+    </thead><tbody>
     { games && games.map(game => {
-      return (<div className="game-element" key={ game.id }>
-        <Link to={ "/game/" + game.id }>
-          <div className="game-item">
-            <div className="title"> { game.title } </div>
-            <div className="create-date"> { game.created.toDate().toLocaleDateString() }</div>
-            <div className="author"> { game.authorNick }</div>
-            <div className="num-of-user">
-              <i className="far fa-user-circle"/>
-              <i className="far fa-user-circle"/>
-              <i className="far fa-user-circle"/>
-              <p className="num-of-user-more"> +5</p>
-            </div>
-            <div className="num-of-post"> 16</div>
-            <div className="num-of-event"> 4</div>
-          </div>
-        </Link>
-      </div>)
+      return (<tr className="game-element" key={ game.id }>
+            <td className="title">
+              <Link to={ "/game/" + game.id }>
+                { game.title }
+              </Link>
+            </td>
+            <td className="create-date"> { game.created.toDate().toLocaleDateString() }</td>
+            <td className="author"> { game.authorNick }</td>
+      </tr>)
     })}
-  </div>);
+  </tbody></table>);
 };
 
 export default GameList;
