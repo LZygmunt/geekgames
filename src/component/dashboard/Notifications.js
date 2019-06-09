8bc6770f4cb6a329f95f9ac3fe1e261e0c245735
Lzygmunt <lzygmunt0@gmail.com>
2019-06-09 12:11:22 +0200

    powiadomienia cz.1

--- src/component/dashboard/Notifications.js
+++ src/component/dashboard/Notifications.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Notifications = ({ message }) => {
+const Notifications = ({ notification, message }) => {
+  let msg, collection;
+  switch (notification && notification.message) {
+    case "games":
+      msg = "Gra";
+      collection = notification.message.slice(0, -1);
+      break;
+    case "events":
+      msg = "Wydarzenie";
+      collection = notification.message.slice(0, -1);
+      break;
+    case "posts":
+      msg = "Do gry";
+      collection = "game";
+      break;
+    case "comments":
+      msg = "Do wydarzenia";
+      collection = "event";
+      break;
+    default:
+      break;
+  }
+  
   return (
     <div className="notification">
-      <p>{ message }</p>
+      {
+        message ?
+          <p> { message }</p>:
+          <p>
+            { msg }&nbsp;
+            <Link to={`/${ collection }/${ notification.objectId }`}>
+              "{ notification.objectTitle }"
+            </Link>&nbsp;
+            { notification.content }
+          </p>
+      }
     </div>
   )
 };
